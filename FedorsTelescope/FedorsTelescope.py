5c4a05235c0f1fbca5401e4814f542538fcd9993
Kaffeedor <49571205+Kaffeedor@users.noreply.github.com>
2020-07-14 21:44:38 +0200

    Add files via upload

---
+++ FedorsTelescope/FedorsTelescope.py
@@ -0,0 +1,65 @@
+import discord
+from time import sleep
+from os import system
+from os import remove
+
+class MyClient(discord.Client):
+    
+    async def on_ready(self):
+        print("I am Logged in. Beep Bob.")
+        await client.change_presence(activity=discord.Game("ft!observe [sec] [nchan]"), status=discord.Status.online)
+   
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith("ft!observe"):
+            #2343.75; 1171.875; 4687,5
+            obst = message.content.split(" ")[1]
+            try:
+                nchan = message.content.split(" ")[2]
+            except:
+                nchan=""
+            
+            try:
+                remove('observation.dat')
+                remove('plot.png')
+            except OSError:
+                pass
+            await message.channel.send("Observation Starting Now")
+            await client.change_presence(activity=discord.Game("Observing for " + str(obst) + " sec"), status=discord.Status.online)
+            
+            if nchan == "":
+                nchan="1024"
+            else:
+                pass
+            
+            if nchan == "512":
+                system("python top_block.py --c-freq=1420e6 --samp-rate=2.4e6 --nchan=512 --nbin=4688 --obs-time="+str(obst))
+                system("python plot_hi.py freq=1420e6 samp_rate=2.4e6 nchan=512 nbin=4688")
+            elif nchan == "1024":
+                system("python top_block.py --c-freq=1420e6 --samp-rate=2.4e6 --nchan=1024 --nbin=2344 --obs-time="+str(obst))
+                system("python plot_hi.py freq=1420e6 samp_rate=2.4e6 nchan=1024 nbin=2344")
+            elif nchan == "2048":
+                system("python top_block.py --c-freq=1420e6 --samp-rate=2.4e6 --nchan=2048 --nbin=1172 --obs-time="+str(obst))
+                system("python plot_hi.py freq=1420e6 samp_rate=2.4e6 nchan=2048 nbin=1172")
+            else:
+                await message.channel.send("(You need to use 512, 1024 or 2048 for nchan!)")
+            
+            sleep(2)
+            print("obs done")
+            try:
+                await message.channel.send("Your Observation is here. Fresh and Tasty!", file=discord.File("plot.png"))
+            except:
+                await message.channel.send("Couldn't find plot.png (or some other random error?!)")
+            await client.change_presence(activity=discord.Game("ft!observe [sec] [nchan]"), status=discord.Status.online)
+            
+        
+        else:
+            pass
+
+
+
+
+client = MyClient()
+client.run("NzMxNzk1MDk4NjIyMjMwNTY5.XwtVpQ.Lks2GT_68xAHuCN5qIuMxCvnfmg")
---
+++ RAB/rab.py
@@ -0,0 +1,82 @@
+import discord
+from time import sleep
+import asyncio as asy
+from random import randint as rain
+
+
+class MyClient(discord.Client):
+    
+    async def on_ready(self):
+        print("I am Logged in. Beep Bob.")
+        while True:
+            await client.change_presence(activity=discord.Game("rab!help"), status=discord.Status.online)
+            
+            
+###############################COMANDS##################################
+    async def on_message(self, message):
+        #print("Message by " + str(message.author) + " enthält " + str(message.content))
+        if message.author == client.user:
+            return
+
+        if message.content.startswith("Hello"):
+            await message.channel.send("Hello there!", tts=True)
+            sleep(1)
+            await message.channel.send("https://tenor.com/view/grevious-general-kenobi-star-wars-gif-11406339")
+
+        elif message.content.startswith("rab!help"):
+            embedd = discord.Embed(title="__**Help**__", colour=discord.Colour.blue())
+            embedd.set_footer(text="Radio Astronomy Bot | made by Kaffeedor#0487 | 2020")
+            embedd.add_field(name="rab!help", value="To display all Commands", inline=False)
+            embedd.add_field(name="Hello", value="Secret", inline=False)
+            await message.channel.send(embed=embedd)
+
+        
+        else:
+            pass
+
+            
+#########################################################################
+
+            
+##########################moderation#####################################
+    async def on_typing(self, channel, user, when):
+        pass
+
+    async def on_message_delete(self, message):
+        channel = client.get_channel(657875763546161153)
+        await channel.send("**Deleted message:** `" + message.content + "` **by:** `" + str(message.author) + "`")
+
+    async def on_message_edit(self, before, after):
+        if str(after.author) != "Radio Astronomy Bot#0553":
+            channel = client.get_channel(657875763546161153)
+            embedd = discord.Embed(title="__**Message Edited**__", description="A Message got Edited", colour=discord.Colour.green())
+            embedd.set_footer(text="Radio Astronomy Bot | made by Kaffeedor#0487 | 2020")
+            embedd.add_field(name="Information", value="**ID:** `"+str(before.id)+"`**; Channel:** `"+str(before.channel)+"` **; Author:** `" + str(after.author)+"`", inline=False)
+            embedd.add_field(name="**Changed Message Before:**", value="`"+before.content+"`", inline=True)
+            embedd.add_field(name="‏‏‎ ‏‏‎", value="-->", inline=True)
+            embedd.add_field(name="**Changed Message After:**", value="`"+after.content+"`", inline=True)
+            await channel.send(embed=embedd)
+        else:
+            pass
+
+    async def on_member_join(self, member):
+        channel = client.get_channel(657875763546161153)
+        await channel.send(str(member) + " has joined!")
+    
+    async def on_member_remove(self, member):
+        channel = client.get_channel(657875763546161153)
+        await channel.send(str(member) + " has left!")
+    
+#    async def on_member_update(self, before, after):
+ #       channel = client.get_channel(657875763546161153)
+  #      await channel.send("`" + "a user..." + "` **changed nickname from** `" + str(before.nick) + "` **to** `" + str(after.nick) + "`")
+        
+    
+#########################################################################   
+                                
+
+
+        
+          
+client = MyClient()
+client.run("NzI5MjQzMzA2NzY4MzM0ODY4.XwGG8A.Am8Uw_TuyJ35YByyaGwl6J8frKI")
