980064fb4bcd79d43c65d84105f1bfcd063c4850
Jacob Bingham <41303821+Zi7ar21@users.noreply.github.com>
2020-07-22 14:30:10 -0600

    Apply Cleaning
    Removed Unnecesary Spaces and Fixed Small Grammaer Issues

--- FedorsTelescope/FedorsTelescope.py
+++ FedorsTelescope/FedorsTelescope.py
@@ -4,36 +4,36 @@ from os import system
 from os import remove
 
 class MyClient(discord.Client):
-    
+
     async def on_ready(self):
         print("I am Logged in. Beep Bob.")
         await client.change_presence(activity=discord.Game("ft!observe [sec] [nchan]"), status=discord.Status.online)
-   
+
     async def on_message(self, message):
         if message.author == client.user:
             return
 
         if message.content.startswith("ft!observe"):
-            #2343.75; 1171.875; 4687,5
+            # 2343.75; 1171.875; 4687, 5
             obst = message.content.split(" ")[1]
             try:
                 nchan = message.content.split(" ")[2]
             except:
                 nchan=""
-            
+
             try:
                 remove('observation.dat')
                 remove('plot.png')
             except OSError:
                 pass
-            await message.channel.send("Observation Starting Now")
+            await message.channel.send("Observation Starting Now!")
             await client.change_presence(activity=discord.Game("Observing for " + str(obst) + " sec"), status=discord.Status.online)
-            
+
             if nchan == "":
-                nchan="1024"
+                nchan="512"
             else:
                 pass
-            
+
             if nchan == "512":
                 system("python top_block.py --c-freq=1420e6 --samp-rate=2.4e6 --nchan=512 --nbin=4688 --obs-time="+str(obst))
                 system("python plot_hi.py freq=1420e6 samp_rate=2.4e6 nchan=512 nbin=4688")
@@ -44,27 +44,24 @@ class MyClient(discord.Client):
                 system("python top_block.py --c-freq=1420e6 --samp-rate=2.4e6 --nchan=2048 --nbin=1172 --obs-time="+str(obst))
                 system("python plot_hi.py freq=1420e6 samp_rate=2.4e6 nchan=2048 nbin=1172")
             else:
-                await message.channel.send("(You need to use 512, 1024 or 2048 for nchan!)")
-            
+                await message.channel.send("(You need to use `512`, `1024` or `2048` for `nchan`!)")
+
             sleep(2)
             print("obs done")
             try:
                 await message.channel.send("Your Observation is here. Fresh and Tasty!", file=discord.File("plot.png"))
             except:
-                await message.channel.send("Couldn't find plot.png (or some other random error?!)")
+                await message.channel.send("Couldn't find `plot.png` (or some other random error?!)")
             await client.change_presence(activity=discord.Game("ft!observe [sec] [nchan]"), status=discord.Status.online)
 
         else:
             pass
-        
-#        if message.content.startswith("ft!cancel"):        DOESNT WORK
+
+#        if message.content.startswith("ft!cancel"):    # Doesn't Work Currently
 #            system("pkill -f top_block.py")
-#
+
 #        else:
 #            pass
-
-
-
 
 client = MyClient()
 client.run("TOKEN")
